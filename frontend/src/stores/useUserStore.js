77dc6455817a0e6f62f405effd61c1b6a083ce45
Mohammed Abdul Raheem <maraheem812@gmail.com>
2025-08-24 16:05:56 +0530

    vercel deployemnt

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import axios from "../lib/axios";
 
-export const useProductStore = create((set) => ({
+export const useUserStore = create((set) => ({
 	products: [],
 	loading: false,
 
